888d9c4da0b91cd233b3123d2d0c49f6007cbee2
Kayla Cloud <kaylacloud27@gmail.com>
2024-01-12 00:11:08 +0000

    Prep-html-script-tag exercise completed.

--- prep-html-script-tag/main.js
+++ prep-html-script-tag/main.js
@@ -1,9 +1,1 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title> HTML script Tag </title>
-  </head>
-  <body>
-console.log("I have added a script file via the src attribute!")
-</body>
-</html>
+console.log ("I have added a script file via the src attribute!");
